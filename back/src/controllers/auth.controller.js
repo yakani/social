29992035b60effbb203e3b8ac210745c4773c31a
yakamkemajoujeanpierredimitri <yakanidamai@gmail.com>
2025-08-27 15:26:13 +0200

    'google'

--- back/src/controllers/auth.controller.js
+++ back/src/controllers/auth.controller.js
@@ -1,23 +1,44 @@
 import generatetoken from '../midlleware/generate.token.js';
 import asyncHandler from 'express-async-handler';
 import generateTokenApp from '../midlleware/generte.app.token.js';
+import User from '../models/user.model.js';
+import { OAuth2Client } from 'google-auth-library';
 const redirectClient = asyncHandler(async(req,res)=>{
-    const app = req.body?.app || null;
-    if(app){
-        const token = generateTokenApp(req.user._id);
-        return res.status(200).json({
-            _id:req.user._id,
-            name:req.user.name,
-            avatar:req.user.avatar,
-            email:req.user.email,
-            role:req.user.role,
-            accessToken:token.accessToken,
-            refreshToken:token.refreshToken
-        });
-    }
     generatetoken(res,req.user._id);
     res.redirect(`${process.env.CLIENT_URL}`)
 });
+const googleTokenAuth = async (req, res) => {
+  const { accessToken } = req.body;
+  const client = new OAuth2Client(process.env.clientID);
+
+  try {
+    const ticket = await client.verifyIdToken({
+      idToken: accessToken,
+      audience: process.env.clientID,
+    });
+    const payload = ticket.getPayload();
+    const email = payload.email;
+    let user = await User.findOne({ email });
+    if (!user) {
+      user = await User.create({
+        email,
+        name: payload.name,
+        password: "password",
+      });
+    }
+    const token = generateTokenApp(user._id);
+    user = {
+        ...user,
+        accessToken:token.accessToken,
+        refreshToken:token.refreshToken
+    };
+    // You may want to generate a JWT or session here
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(401).json({ message: "Invalid Google token", error });
+  }
+};
 export {
-    redirectClient
+    redirectClient,
+    googleTokenAuth
 }
--- back/src/routes/auth.route.js
+++ back/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { redirectClient} from "../controllers/auth.controller.js";
+import { googleTokenAuth, redirectClient} from "../controllers/auth.controller.js";
 import passport from 'passport';
 import '../lib/google.js';
 const router = Router();
@@ -7,4 +7,5 @@ router.get('/google',passport.authenticate('google'));
 router.get('/google/callback', passport.authenticate('google', {
     failureRedirect: `${process.env.CLIENT_URL}/login`,
   }), redirectClient);
+  router.post('/google/callback',googleTokenAuth);
   export default router;
