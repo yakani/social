dadd904e0a0f18e6aa0062ce1fde24386ddbc507
yakamkemajoujeanpierredimitri <yakanidamai@gmail.com>
2025-09-25 11:11:50 +0200

    'bigfiles'

--- back/src/lib/Moderation.js
+++ back/src/lib/Moderation.js
@@ -3,14 +3,11 @@ import FormData from "form-data";
 
 const MAX_DIRECT_SIZE = 50 * 1024 * 1024; // 50MB limit for direct API
 
-async function handleLargeFile(buffer, name, mimetype, userId, apiSecret, duration) {
-  const uploadData = new FormData();
-  uploadData.append("media", buffer, { filename: name, contentType: mimetype });
-  uploadData.append("api_user", userId);
-  uploadData.append("api_secret", apiSecret);
+async function handleLargeFile(buffer, name, mimetype, userId, apiSecret, duration ,size) {
+
 
   // First upload the file
-  const uploadResponse = await axios.get(
+  /*const uploadResponse = await axios.get(
     "https://api.sightengine.com/1.0/upload/create-video.json",
     {
       params: {
@@ -29,22 +26,24 @@ const uploadFileResponse = await axios.put(uploadUrl, buffer, {
       maxContentLength: Infinity,
       maxBodyLength: Infinity,
       timeout: 300000 // 5 minutes timeout
-    });
+    });*/
   // Now moderate using the uploaded file's URL
   const moderationData = new FormData();
-  moderationData.append("media_id", mediaId);
+  const slicedBuffer = buffer.slice(0, MAX_DIRECT_SIZE);
+  moderationData.append("media_id", slicedBuffer, { filename: name, contentType: mimetype });
   moderationData.append("workflow", mimetype.startsWith("video/")
     ? "wfl_iVgsawcIGihrv6l7e6ziF"
     : "wfl_iVgpt3JDFmBOfzFfRPGR1");
   moderationData.append("api_user", userId);
   moderationData.append("api_secret", apiSecret);
-
-  if (duration > 60) {
+// Calculate adjusted duration based on the ratio of sliced content
+const adjustedDuration = Math.floor(duration * (MAX_DIRECT_SIZE / size));
+  if (adjustedDuration > 60) {
     moderationData.append("callback_url", `${process.env.URL}/post/moderation`);
   }
 
   const moderationResponse = await axios.post(
-    `https://api.sightengine.com/1.0/${mimetype.startsWith("video/") ? "video/" : ""}${duration == 0 || duration > 60 ? "check-workflow" : "check-workflow-sync"}.json`,
+    `https://api.sightengine.com/1.0/${mimetype.startsWith("video/") ? "video/" : ""}${adjustedDuration == 0 || adjustedDuration > 60 ? "check-workflow" : "check-workflow-sync"}.json`,
     moderationData,
     { headers: moderationData.getHeaders() }
   );
@@ -77,7 +76,7 @@ async function handleSmallFile(buffer, name, mimetype, userId, apiSecret, durati
 export async function moderateMedia(buffer, name, mimetype, userId, apiSecret, duration, length = 0) {
   try {
     if (length > MAX_DIRECT_SIZE) {
-      return await handleLargeFile(buffer, name, mimetype, userId, apiSecret, duration);
+      return await handleLargeFile(buffer, name, mimetype, userId, apiSecret, duration ,length);
     } else {
       return await handleSmallFile(buffer, name, mimetype, userId, apiSecret, duration);
     }
